a6d5c4da069bfa049d0ba68b36aa12b46f98757d
charlottekeuer <139115048+uetrozi@users.noreply.github.com>
2024-01-15 17:02:30 +0100

    add list Container with styling

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 "use client";
 import Head from "next/head";
+import { Heading } from "@chakra-ui/react";
+
 import { Inter } from "next/font/google";
 import { Link } from "@chakra-ui/next-js";
-import InputForm from "@/components/inputForm";
+import ListContainer from "@/components/ListContainer/ListContainer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const listTitle = "Today";
+
   return (
     <>
       <Head>
-        <title>To Do</title>
+        <title>TaskTango</title>
       </Head>
-      <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
-        About
-      </Link>
-      <InputForm></InputForm>
+      <Heading p="6" >Upcoming</Heading>
+
+      <ListContainer listTitle={listTitle} />
     </>
   );
 }
