9db1e8b7b0ea4182c4ac5d283e7dc86e43fa836f
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-15 17:05:46 +0100

    adding tasks list

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,8 +1,9 @@
-import { Checkbox, CheckboxGroup, Spinner, Stack } from "@chakra-ui/react";
 import useSWR from "swr";
+import { Checkbox, ListItem, Spinner, UnorderedList } from "@chakra-ui/react";
 
 export default function TaskList() {
-  const { data, isLoading } = useSWR("/api/tasks");
+  const { data, isLoading, error } = useSWR("/api/tasks");
+  console.log(data);
 
   if (isLoading) {
     return (
@@ -13,19 +14,23 @@ export default function TaskList() {
     );
   }
 
+  if (error) {
+    return <div>failed to load</div>;
+  }
+
   if (!data) {
     return;
   }
+
   return (
-    <>
-      <CheckboxGroup colorScheme="green">
-        <Stack spacing={[1, 5]} direction={["column", "row"]}></Stack>
-        {data.map((task) => (
-          <li key={task._id}>
-            <Checkbox href={`/${task._id}`}>{task.title}</Checkbox>
-          </li>
-        ))}
-      </CheckboxGroup>
-    </>
+    <UnorderedList styleType="none" colorScheme="green.500" spacing={3}>
+      {data.map((task) => (
+        <ListItem key={task._id}>
+          <Checkbox key={task._id} href={`/${task._id}`}>
+            {task.title}
+          </Checkbox>
+        </ListItem>
+      ))}
+    </UnorderedList>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 "use client";
 import Head from "next/head";
+import { SWRConfig } from "swr";
 import { Inter } from "next/font/google";
 import { Link } from "@chakra-ui/next-js";
 import InputForm from "@/components/inputForm";
@@ -8,16 +9,20 @@ import TaskList from "@/components/TaskList";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const fetcher = (url) => fetch(url).then((response) => response.json());
+
   return (
     <>
-      <Head>
-        <title>To Do</title>
-      </Head>
-      <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
-        About
-      </Link>
-      <InputForm></InputForm>
-      <TaskList />
+      <SWRConfig value={{ fetcher }}>
+        <Head>
+          <title>To Do</title>
+        </Head>
+        <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
+          About
+        </Link>
+        <InputForm></InputForm>
+        <TaskList />
+      </SWRConfig>
     </>
   );
 }
