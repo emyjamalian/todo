89b47e8736a6b5d50171626976587fb449b48c4d
charlottekeuer <139115048+uetrozi@users.noreply.github.com>
2024-01-16 17:33:13 +0100

    add Zustand store and it works yay

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
 "use client";
 import Head from "next/head";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { SWRConfig } from "swr";
-import { Heading } from "@chakra-ui/react";
-import { Inter } from "next/font/google";
-
+import useSWR from "swr";
+import { useEffect } from "react";
 import ListContainer from "@/components/ListContainer/ListContainer";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useTaskStore } from "@/store";
 
 export default function Home() {
   const fetcher = (url) => fetch(url).then((response) => response.json());
+
+  const { setData, isLoading, error } = useTaskStore();
+
+  const { data: swrData, error: swrError } = useSWR("/api/tasks", fetcher);
+
+  useEffect(() => { if (swrData) { setData(swrData); }
+}, [swrData, setData]);
+
+  if (swrError) {
+    return <div>Failed to load</div>;
+  }
+
+  if (isLoading || !swrData) {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <Spinner size="xl" />
+      </>
+    );
+  }
+
+  
 
   const listTitle = "Today";
 
