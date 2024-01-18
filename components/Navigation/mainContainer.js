a86302ead11ef2fa7ad03c459fb0416591cdce8c
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-18 18:57:58 +0100

    apply styles

--- components/Navigation/mainContainer.js
+++ components/Navigation/mainContainer.js
@@ -1,12 +1,14 @@
 import { Container, Heading, Box } from "@chakra-ui/react";
+import ListContainer from "../ListContainer/ListContainer";
 
 export default function MainContainer({ mainTitle }) {
   return (
-    <Box p="6" border="2px" borderColor="gray.300" rounded="2xl">
+    <Box p="6" color="black" border="2px" borderColor="gray.300" rounded="2xl">
       <Container>
         <Heading paddingbottom="5" as="h3" size="3xl" mb={5} noOfLines={1}>
           {mainTitle}
         </Heading>
+        <ListContainer />
       </Container>
     </Box>
   );
