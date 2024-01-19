bae2943893e2e26fae7369fff36f23d3b89c8f5d
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-19 10:52:55 +0100

    added delete and edit button

--- components/TaskList/TaskList.jsx
+++ components/TaskList/TaskList.jsx
@@ -1,16 +1,40 @@
-import { Checkbox, ListItem, UnorderedList } from "@chakra-ui/react";
+import {
+  Checkbox,
+  ListItem,
+  UnorderedList,
+  IconButton,
+  Spacer,
+  Divider,
+  Flex,
+} from "@chakra-ui/react";
 import { useTaskStore } from "@/store";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 export default function TaskList() {
   const { data: tasks } = useTaskStore();
   console.log("DEBUG LIST: ", tasks);
   return (
-    <UnorderedList styleType="none" colorScheme="green.500" spacing={3}>
+    <UnorderedList styleType="none" spacing={5}>
       {tasks.map((task) => (
-        <ListItem key={task._id}>
-          <Checkbox key={task._id} href={`/${task._id}`}>
-            {task.title}
-          </Checkbox>
+        <ListItem key={task._id} w="100%">
+          <Flex>
+            <Checkbox key={task._id} href={`/${task._id}`}>
+              {task.title}
+            </Checkbox>
+            <Spacer />
+            <IconButton
+              aria-label="Delete a task"
+              size="xs"
+              margin="0 5px "
+              icon={<EditIcon />}
+            />
+            <IconButton
+              aria-label="Delete a task"
+              size="xs"
+              icon={<DeleteIcon />}
+            />
+          </Flex>
+          <Divider />
         </ListItem>
       ))}
     </UnorderedList>
