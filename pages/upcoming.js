59f179f078820528abcb92540239c9bbd9f9966a
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-23 13:44:14 +0100

    added use client

--- pages/upcoming.js
+++ pages/upcoming.js
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import Layout from "@/components/Layout/Layout";
 import MainContainer from "@/components/Navigation/mainContainer";
 import useSWR from "swr";
