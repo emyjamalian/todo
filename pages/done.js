500659f52ab881b258fe30b41a9631adae316a72
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-23 12:17:02 +0100

    Updated done page and added a new endpoint for done list

--- pages/done.js
+++ pages/done.js
@@ -1,12 +1,36 @@
-// pages/done.js
 import React from "react";
 import Layout from "@/components/Layout/Layout";
 import MainContainer from "@/components/Navigation/mainContainer";
+import useSWR from "swr";
+import { Spinner } from "@chakra-ui/react";
 
 const DonePage = () => {
+  const { data: doneTasks, isLoading, error } = useSWR("/api/status/done");
+
+  console.log("done tasks", doneTasks);
+
+  if (isLoading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <Spinner size="xl" />
+      </>
+    );
+  }
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+
+  if (!doneTasks) {
+    return;
+  }
+
   return (
     <Layout title="TaskTango - Done">
-      <MainContainer mainTitle="Done" />
+      <MainContainer mainTitle="Done">
+        <TaskList tasks={doneTasks} />
+      </MainContainer>
     </Layout>
   );
 };
