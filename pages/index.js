31201b3fea1ea824308f6e4e32bc2ddba72fba32
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-23 12:16:25 +0100

    Updated Home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
-// pages/index.js
 import React from "react";
 import Layout from "@/components/Layout/Layout";
 import MainContainer from "@/components/Navigation/mainContainer";
+import InputForm from "@/components/Task/AddTaskInput";
+import TaskList from "@/components/TaskList/TaskList";
+import useSWR from "swr";
+import { Spinner } from "@chakra-ui/react";
 
 const IndexPage = () => {
+  const { data: tasks, isLoading, error } = useSWR("/api/tasks");
+
+  console.log("all tasks", tasks);
+
+  if (isLoading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <Spinner size="xl" />
+      </>
+    );
+  }
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+
+  if (!tasks) {
+    return;
+  }
+
   return (
-    <Layout title="TaskTango - Upcoming">
-      <MainContainer mainTitle="Upcoming" flex="1" />
+    <Layout title="TaskTango - Home Page">
+      <MainContainer mainTitle="Add a new task" flex="1">
+        <InputForm />
+        <TaskList tasks={tasks} />
+      </MainContainer>
     </Layout>
   );
 };
