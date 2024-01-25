668c02ac29059b7eb794d2475b37c44973ae8838
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-25 15:55:23 +0100

    updated the style and hid the fun mode 🎉

--- components/Navigation/MenuContainer.jsx
+++ components/Navigation/MenuContainer.jsx
@@ -5,6 +5,7 @@ import {
   Spacer,
   Flex,
   useColorModeValue,
+  Divider,
 } from "@chakra-ui/react";
 import Search from "./Search";
 import ListsSection from "./ListsSection";
@@ -15,8 +16,8 @@ export default function MenuContainer() {
   const bg = useColorModeValue("gray.200", "gray.700");
 
   return (
-    <Box p="4" maxWidth="346px" borderRadius="50px" bg={bg}>
-      <Flex direction="column">
+    <Box p="4" maxWidth="346px" minH="500px" borderRadius="50px" bg={bg}>
+      <Flex direction="column" justifyContent="space-between">
         <Box p="4">
           <Heading paddingbottom="5" as="h5" size="2xl" mb={5} noOfLines={1}>
             Menu
@@ -25,9 +26,12 @@ export default function MenuContainer() {
           <Search />
           <ListsSection />
         </Box>
-        <Spacer />
-        <DarkMode />
-        <FunMode />
+
+        <Box margin="50px 20px">
+          <DarkMode />
+          <Divider mb={1} />
+          {/* <FunMode /> */}
+        </Box>
       </Flex>
     </Box>
   );
