ac0a228b02e137b33160b83a0cb31ea3acc8d467
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-25 22:29:50 +0100

    fun for micha

--- components/Navigation/MenuContainer.jsx
+++ components/Navigation/MenuContainer.jsx
@@ -30,7 +30,7 @@ export default function MenuContainer() {
         <Box margin="50px 20px">
           <DarkMode />
           <Divider mb={1} />
-          {/* <FunMode /> */}
+          <FunMode />
         </Box>
       </Flex>
     </Box>
