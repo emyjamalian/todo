69a0ecfc53fc8ddb84d24a111ba2d3e5c670ccf5
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-25 12:02:27 +0100

    added e2e test for adding an item and making sure it exist in the list

---
+++ tests/e2e/tests/e2e-test.spec.js
@@ -0,0 +1,32 @@
+import randomItem from "./utils/utils";
+const { test, expect } = require("@playwright/test");
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://tasktango.vercel.app/");
+});
+
+test.describe("New Todo", () => {
+  test("should allow me to add todo items", async ({ page }) => {
+    await expect(page).toHaveTitle("TaskTango - Home Page");
+
+    // Wait for the new task input to appear
+    const newTaskInput = await page.waitForSelector(
+      'input[placeholder="Add new task"]'
+    );
+
+    // Create 1st todo.
+    const todoText = randomItem;
+    await newTaskInput.fill(todoText);
+    await newTaskInput.press("Enter");
+
+    // Wait for the todo list to appear
+    const todoListSelector = ".css-bs4x2b";
+    await page.waitForSelector(todoListSelector);
+
+    // Assert that the added todo item is in the list
+    const addedTodoSelector = `${todoListSelector} .css-8atqhb:has-text("${todoText}") .chakra-editable__preview`; // Adjust with the actual selector
+    await page.waitForSelector(addedTodoSelector);
+    const addedTodoText = await page.textContent(addedTodoSelector);
+    expect(addedTodoText).toContain(todoText);
+  });
+});
