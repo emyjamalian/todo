75a17fe2f750b636df13c142b2a06734dc6c883c
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-02-04 12:13:00 +0100

    removed extra comments

--- tests/e2e/tests/e2e-test.spec.js
+++ tests/e2e/tests/e2e-test.spec.js
@@ -3,52 +3,59 @@ const { test, expect } = require("@playwright/test");
 
 test.beforeEach(async ({ page }) => {
   await page.goto("https://tasktango.vercel.app/");
+  await expect(page).toHaveTitle("TaskTango - Home Page");
 });
 
 test.describe("New Todo", () => {
   test("Add a task and verify it appears in the list", async ({ page }) => {
-    await expect(page).toHaveTitle("TaskTango - Home Page");
-
     // Wait for the new task input to appear
     const newTaskInput = await page.waitForSelector(
       'input[placeholder="Add new task"]'
     );
-
     // Create 1st todo.
     const todoText = randomItem;
     await newTaskInput.fill(todoText);
     await newTaskInput.press("Enter");
 
-    // Wait for the todo list to appear and it has the item
-    // const todoList = page.locator(".chakra-stack", {
-    //   has: page.getByText(todoText),
-    // });
-
+    //find task in the tasklist
     const ListItem = page.getByRole("listitem").filter({ hasText: todoText });
 
     //mark item as done and assert it's checked
     const itemCheckbox = ListItem.locator(".chakra-checkbox__control");
 
-    await itemCheckbox.click();
+    //mark the latest as done even if there are multiple ones
+    await itemCheckbox.first().click();
     expect(itemCheckbox).toBeTruthy();
 
     //assert the toast is showing for task is done
-    expect(page.getByText("Task Done")).toBeVisible();
+    await expect(page.getByText("Task Done")).toBeVisible();
+  });
+
+  test("Add a task and Delete it and verify it appears in the list", async ({
+    page,
+  }) => {
+    // Wait for the new task input to appear
+    const newTaskInput = await page.waitForSelector(
+      'input[placeholder="Add new task"]'
+    );
+    // Create 2st todo.
+    const todoText2 = randomItem;
+    await newTaskInput.fill(todoText2);
+    await newTaskInput.press("Enter");
+
+    //find task in the tasklist
+    const ListItem2 = page.getByRole("listitem").filter({ hasText: todoText2 });
 
     //delete a task and assert it's deleted
-    const itemDeleteBtn = ListItem.locator(
+    const itemDeleteBtn = ListItem2.locator(
       'button[aria-label="Delete a task"]'
     );
 
-    await itemDeleteBtn.click();
-    // expect(todoList).not.toHaveText(todoText);
+    //delete on the latest added even if there are multiple ones
+    await itemDeleteBtn.first().click();
 
-    const deleteToast = page.locator(
-      'region[aria-label="Notifications-bottom"]'
-    );
-    //assert the toast is showing for task is done
-    expect(deleteToast).toBeVisible();
-
-    expect(page.getByText("Task deleted")).toBeInViewport();
+    //assert the toast is showing for task is deleted
+    await expect(page.getByText("Task deleted")).toBeInViewport();
+    //await expect(ListItem2).not.toBeVisible();
   });
 });
